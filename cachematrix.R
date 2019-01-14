8d15b5dda40fe4a6e4d0a2fb78bd03b08c826ab0
Isabella Salmi <isalmi@Market-Bridge.com>
2019-01-14 12:48:23 -0500

    filled in cachematrix.R with solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## isalmi Coursera Data Science Programming Assignment 2
 
-## Write a short comment describing this function
+## Calculate and cache the inverse of a square matrix
+## Create a makeCacheMatrix() object with a matrix x as its argument
+## Call cacheSolve() on this mCM to calculate the inverse of x or retrieve the cached inverse
 
+## Requires a matrix x
+## Creates a set of functions to cache and retrieve x and its inverse
+## Returns a named list of the functions
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## instantiate the inverse as NULL
+  inv <- NULL
+  
+  ## set() resets the vector object for this cache in the parent 
+  ## environment and resets the inverse to NULL
+  set<- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  
+  ## get() retrieves the vector from the parent environment
+  get <- function() x
+  
+  ## setinvers() sets the parent enviroment's inv variable to the
+  ## input matrix
+  setinverse <- function(inverse) inv <<- inverse
+  
+  ## getinverse() retrieves cached inverse matrix from the parent envi
+  getinverse <- function() inv
+  
+  ## return a list of named function objects
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
 
+## Requires a makeCacheMatrix object x
+## Returns the inverse of the matrix that is cached in x
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## retrieve the cached inverse from the mCM object
+  inv <- x$getinverse()
+  
+  ## if inv is not null, return the inverse matrix
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  
+  ## if inv is null, calculate the inverse of the matrix cached in 
+  ## the mCM object, cache that inverse, and return it
+  data <- x$get()
+  inv <- solve(data)
+  x$setinverse(inv)
+  inv
 }
